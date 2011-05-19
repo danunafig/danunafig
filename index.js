69109124d0dd60cec56d36088db2c153a21f639f
Denis <devel@danunafig.com>
2011-05-18 22:26:44 -0400

    secoтв to go

---
+++ index.js
@@ -0,0 +1,6 @@
+var   http     = require("http"),
+      url      = require("url"),
+      path     = require("path"),
+      fs       = require("fs"),
+      events   = require("events"),
+      sys      = require("sys");
